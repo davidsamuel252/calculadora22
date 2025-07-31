6d026ccb9096b6fed2276f20f40bd7bcdeb2e322
davidsamuel252 <david.becker.silva@escola.pr.gov.br>
2025-07-31 11:43:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function soma(){
+function calculo(){
 
-    let c = parseInt(document.getElementById('numero_')
+let C = parseInt(document.getElementById('primeiro_numero').value);
+let I = parseInt(document.getElementById('segundo_numero').value);
+let T = parseInt(document.getElementById('terceiro_numero').value);
+I = I/100;
+let soma_numero = C * I * T;
+document.getElementById("resultado").textContent= 'total dos juros é: ' + soma_numero;
+let M = parseInt(document.getElementById('quarto_numero').value);
+M = soma_numero + C;
+let soma_numero2 = M;
+document.getElementById("quarto_numero").textContent= 'e o montante:' + soma_numero2;
 }
-
